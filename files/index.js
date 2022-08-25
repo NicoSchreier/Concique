52953f4f65e0f1437b8fbbf4ba81d6d694ed81d2
Nico Schulterer <91744414+Nico-Schulterer@users.noreply.github.com>
2022-08-25 16:55:54 +0200

    index/landing page updated

--- files/index.js
+++ files/index.js
@@ -13,3 +13,44 @@ fetch('https://instagram-data1.p.rapidapi.com/followers?username=valloho', optio
     .then(response => console.log(response['count']))
     .catch(err => console.error(err));
  */
+
+function mouseOverBar() {
+    document.getElementById("cLogoBar").style.opacity = "1";
+    document.getElementById("barText").style.opacity = "1";
+    document.getElementById("barImg").style.transform = "scale(1.25)";
+    document.getElementById("barImg").style.filter = "grayscale(0%)";
+}
+
+function mouseOutBar() {
+    document.getElementById("cLogoBar").style.opacity = "0";
+    document.getElementById("barText").style.opacity = "0"
+    document.getElementById("barImg").style.transform = "scale(1)";
+    document.getElementById("barImg").style.filter = "grayscale(100%)";
+}
+
+function mouseOverClub() {
+    document.getElementById("cLogoClub").style.opacity = "1";
+    document.getElementById("clubText").style.opacity = "1";
+    document.getElementById("clubImg").style.transform = "scale(1.25)";
+    document.getElementById("clubImg").style.filter = "grayscale(0%)";
+}
+
+function mouseOutClub() {
+    document.getElementById("cLogoClub").style.opacity = "0";
+    document.getElementById("clubText").style.opacity = "0";
+    document.getElementById("clubImg").style.transform = "scale(1)";
+    document.getElementById("clubImg").style.filter = "grayscale(100%)";
+}
+function mouseOverEvent() {
+    document.getElementById("cLogoEvent").style.opacity = "1";
+    document.getElementById("eventText").style.opacity = "1";
+    document.getElementById("eventImg").style.transform = "scale(1.25)";
+    document.getElementById("eventImg").style.filter = "grayscale(0%)";
+}
+
+function mouseOutEvent() {
+    document.getElementById("cLogoEvent").style.opacity = "0";
+    document.getElementById("eventText").style.opacity = "0";
+    document.getElementById("eventImg").style.transform = "scale(1)";
+    document.getElementById("eventImg").style.filter = "grayscale(100%)";
+}
